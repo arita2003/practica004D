13cafd6e619aaff30b19ea4bb41264c4e7189e9e
enicxus <tineoanderson7@gmail.com>
2023-03-19 23:45:00 -0300

    paginasegu

--- scrip.js
+++ scrip.js
@@ -5,3 +5,11 @@ var typed =new Typed(".multiple-text",{
     backDelay:1000,
     loop:true
 })
+
+var typed =new Typed(".segundo-text",{
+    strings:[ "","Araceli","Fernanda","Anderson"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
